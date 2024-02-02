43d761726fc0f4135783db0e5057d4aec9b64bb8
NIckson223 <nicktyshko23@gmail.com>
2024-02-02 10:04:47 +0200

    Start working on Cart for shop

--- internet_magazin/settings.py
+++ internet_magazin/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+CART_SESSION_ID = 'cart'
 
 STATIC_URL = 'static/'
 
